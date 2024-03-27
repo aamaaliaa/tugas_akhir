2ae53e8536e2aa7af3e92307454506df52aa72b0
aamaaliaa <priliantiniamalia@gmail.com>
2024-03-27 23:31:35 +0700

    tugas pertemuan 5

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php 
+//koneksi ke data 
+$koneksi = mysqli_connect("localhost", "root", "", "mahasiswa");
+
+//ambil data dari tabel dat_mhs /query data matahasiswa
+$result = mysqli_query($koneksi,"SELECT * FROM data_mhs");
+/* var_dump($result);
+
+//ambil data (fetch) dari object result
+//mysqli_fetch_row();
+//mysqli_fecth_assoc();
+//mysqli_fetch_array();
+//mysqli_fetch_object();
+*/
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <table  border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Jurusan</th>
+            <th>Email</th>
+        </tr>
+
+        <?php while($row = mysqli_fetch_assoc($result))  : ?>
+        <tr>
+        <td>1</td>
+        <td>
+            <a href="">ubah</a>
+            <a href="">hapus</a>
+        </td>
+        <td>
+            <img src="ASET/<?= $row[ 'gambar'] ?>" width="50">
+        </td>
+        <td><?= $row['nim'] ?></td>
+        <td><?= $row['nama'] ?></td>
+        <td><?= $row['jurusan'] ?></td>
+        <td><?= $row['email'] ?></td>
+        </tr>
+        <?php endwhile; ?>
+
+    </table>
+</body>
+</html>
